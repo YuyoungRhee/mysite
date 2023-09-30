36c8e466dc1346e19de17ecf44724ecad61627c1
YuyoungRhee <bbclara@naver.com>
2023-10-01 08:27:47 +0900

    5.2까지

--- ch99/books/urls.py
+++ ch99/books/urls.py
@@ -1,8 +1,21 @@
-from django.urls import admin
+from django.urls import path
 from . import views
 
+app_name = 'books'
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls')),
-    path('books/', include('books.urls')),
+    # /books/
+    path('', views.BooksModelView.as_view(), name='index'),
+    # /books/book
+    path('book/', views.BookList.as_view(), name='book_list'),
+
+    path('author/', views.AuthorList.as_view(), name='author_list'),
+    
+    path('publisher', views.PublisherList.as_view(), name='publisher_list'),
+
+    # /books/book/99/
+    path('book/<int:pk>/', views.BookDetail.as_view(), name='book_detail'),
+
+    path('author/<int:pk>/', views.AuthorDetail.as_view(), name='author_detail'),
+
+    path('publisher/<int:pk>/', views.PublisherDetail.as_view(), name='publisher_detail'),
 ]
